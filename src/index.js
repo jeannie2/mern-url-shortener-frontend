6d7d47228f07ae5d2276a0c81150c5555d973a77
jeannie <jnam820@gmail.com>
2023-07-11 15:39:54 +0800

    updated index.html App.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app.css'; // import './index.css';
+import './App.css'; // import './index.css';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
