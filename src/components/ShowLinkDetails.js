fd77618b53faef8e8fb3e600011975de23463f0b
jeannie <jnam820@gmail.com>
2023-07-12 17:55:55 +0800

    button spacing float-left, added text-center for label quote

--- src/components/ShowLinkDetails.js
+++ src/components/ShowLinkDetails.js
@@ -56,7 +56,7 @@ function ShowLinkDetails(props) {
             <div className='col-md-6 mx-auto text-center'>
               <Link
                 to={`/`}
-                className='btn btn-outline-light btn-block mx-2'
+                className='btn btn-outline-light float-left btn-block mx-2'
                 id='shorten-button'
               >
               Shorten new link
